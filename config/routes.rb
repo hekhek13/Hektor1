6d273853b38c61c17716ac2b22fea87f5634ec7b
Hector Hurtado <hekhek13@gmail.com>
2016-06-20 23:11:27 -0700

    updated link for landing pages. ensured link works correctly

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
+  get 'static_pages/landing_page'
+
   get '/products/:id', to: 'products#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
