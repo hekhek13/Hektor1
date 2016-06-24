1c161d58c5423d02924102874c256d22dd81daed
Hector Hurtado <hekhek13@gmail.com>
2016-06-23 22:55:12 -0700

    resolved sending emails in deveveloper mode

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,4 @@
 class UserMailer < ApplicationMailer
-end
-
-class UserMailer < ApplicationMailer
   default from: "from@example.com"
 
   def contact_form(email, name, message)
@@ -10,12 +7,4 @@ class UserMailer < ApplicationMailer
         :to => 'hekhek13@gmail.com',
         :subject => "A new contact form message from #{name}")
   end
-end
-
-
-def thank_you
-  @name = params[:name]
-  @email = params[:email]
-  @message = params[:message]
-  UserMailer.contact_form(@email, @name, @message).deliver_now
 end
