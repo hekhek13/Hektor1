f0d04944cea200667e56a0ab5ee2117892b5c716
Hector Hurtado <hekhek13@gmail.com>
2016-07-13 23:13:13 -0700

    added Automated email on user sign up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,12 @@ class UserMailer < ApplicationMailer
         :to => 'hekhek13@gmail.com',
         :subject => "A new contact form message from #{name}")
   end
+
+  def welcome(user)
+  @appname = "Bike Berlin"
+  mail( :to => user.email,
+        :subject => "Welcome to #{@appname}!")
+  end
+
+
 end
