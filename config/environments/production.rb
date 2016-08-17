7f2fd9ae88af27592515c3f934670ee73939e75b
Hector Hurtado <hekhek13@gmail.com>
2016-08-16 18:58:10 -0700

    configured app to run ActionCable on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.web_socket_server_url = "wss://hekhurtado13.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://hekhurtado13.herokuapp.com', 'http://hekhurtado13.herokuapp.com']
+
 end
