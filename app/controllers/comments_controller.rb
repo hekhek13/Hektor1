0abbbe11f12ecddd316383291bc6fc7289d3f71b
Hector Hurtado <hekhek13@gmail.com>
2016-08-17 20:57:34 -0700

    Authenticated ActionCable ensured comments stream for specific product

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,10 +5,10 @@ class CommentsController < ApplicationController
       @product = Product.find(params[:product_id])
       @comment = @product.comments.new(comment_params)
       @comment.user = current_user
+      @user = current_user
 
       respond_to do |format|
-        if @comment.save
-          ActionCable.server.broadcast 'product_channel', comment: @comment
+        if @comment.save          
           format.html { redirect_to @product, notice: 'Review was created successfully.' }
           format.json { render :show, status: :created, location: @product }
           format.js
